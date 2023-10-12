3efbea12c01a0dab78923d3660d2a1f2beb59d79
Juan <juan.paez@api-ux.com>
2023-10-12 12:57:35 -0500

    pr base de prueba

--- apiCore/v1/index.php
+++ apiCore/v1/index.php
@@ -723,7 +723,8 @@ function generateApiTokenadmin() {
      $query= mysqli_query($conectar,"INSERT INTO owners (ownerId,userId,name,lastName,contact,email) VALUES ('$userid','$userid','$ownerName','$ownerLastName','$contact','$email')");
      $query= mysqli_query($conectar,"INSERT INTO subList (subId,clientId) VALUES ('$subId','$clientId')");
      
-
+     $query= mysqli_query($conectar,"INSERT INTO clientStyle (styleId,clientId) VALUES ('$styleId','$clientId')");
+     
             echo "true|¡Cliente creado con exito!";
         
            // echo json_encode($response1);
@@ -1458,6 +1459,96 @@ Flight::route('GET /getInternalClients/@filter', function ($filter) {
                 $row=$query->fetch_assoc();
                 //echo json_encode($students) ;
                 echo json_encode(['clients'=>$values]);
+          
+               
+           
+
+        } else {
+            echo 'Error: Autenticación fallida';
+             //echo json_encode($response1);
+        }
+    } else {
+        echo 'Error: Encabezados faltantes';
+    }
+});
+
+
+
+Flight::route('GET /getClientStyle/@filter', function ($filter) {
+    header("Access-Control-Allow-Origin: *");
+    // Leer los encabezados
+    $headers = getallheaders();
+    
+    // Verificar si los encabezados 'Api-Key' y 'Secret-Key' existen
+    if (isset($headers['Api-Key']) && isset($headers['x-api-Key'])) {
+        // Leer los datos de la solicitud
+       
+        // Acceder a los encabezados
+        $apiKey = $headers['Api-Key'];
+        $xApiKey = $headers['x-api-Key'];
+        
+        $sub_domaincon=new model_domain();
+        $sub_domain=$sub_domaincon->domKairos();
+        $url = $sub_domain.'/kairosCore/apiAuth/v1/authApiKeyKairos/';
+      
+        $data = array(
+          'apiKey' =>$apiKey, 
+          'xApiKey' => $xApiKey
+          
+          );
+      $curl = curl_init();
+      
+      // Configurar las opciones de la sesión cURL
+      curl_setopt($curl, CURLOPT_URL, $url);
+      curl_setopt($curl, CURLOPT_POST, true);
+      curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+      curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+      // curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+      
+      // Ejecutar la solicitud y obtener la respuesta
+      $response1 = curl_exec($curl);
+
+      
+
+
+      curl_close($curl);
+
+      
+
+        // Realizar acciones basadas en los valores de los encabezados
+
+
+        if ($response1 == 'true' ) {
+           
+
+
+
+           
+            $conectar=conn();
+                $query= mysqli_query($conectar,"SELECT styleId,bgColor,textColor,imgLogo,imgIcon,imgGif,clientId FROM clientStyle WHERE clientId='$filter'");
+           
+            
+          
+                $values=[];
+          
+                while($row = $query->fetch_assoc())
+                {
+                        $value=[
+                            'styleId' => $row['styleId'],
+                            'bgColor' => $row['bgColor'],
+                            'textColor' => $row['textColor'],
+                            'imgLogo' => $row['imgLogo'],
+                            'imgIcon' => $row['imgIcon'],
+                            'imgGif' => $row['imgGif'],
+                            'clientId' => $row['clientId']
+                        ];
+                        
+                        array_push($values,$value);
+                        
+                }
+                $row=$query->fetch_assoc();
+                //echo json_encode($students) ;
+                echo json_encode(['clientStyle'=>$values]);
           
                
            
