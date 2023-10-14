4ff9ea94a45f737390f526fd6e45a2961d240e34
Juan <juan.paez@api-ux.com>
2023-10-14 13:53:07 -0500

    pr base de prueba

--- apiCompanies/v1/index.php
+++ apiCompanies/v1/index.php
@@ -594,6 +594,104 @@ Flight::route('POST /postClientRoom/@apk/@xapk', function ($apk,$xapk) {
 });
 
 
+
+
+
+
+Flight::route('POST /postClientElement/@apk/@xapk', function ($apk,$xapk) {
+  
+    header("Access-Control-Allow-Origin: *");
+    // Verificar si los encabezados 'Api-Key' y 'Secret-Key' existen
+    if (!empty($apk) && !empty($xapk)) {    
+        // Leer los datos de la solicitud
+       
+
+
+
+
+        
+
+
+
+
+        $sub_domaincon=new model_domain();
+        $sub_domain=$sub_domaincon->domKairos();
+        $url = $sub_domain.'/kairosCore/apiAuth/v1/authApiKey/';
+      
+        $data = array(
+            'apiKey' =>$apk, 
+            'xApiKey' => $xapk
+          
+          );
+      $curl = curl_init();
+      
+      // Configurar las opciones de la sesión cURL
+      curl_setopt($curl, CURLOPT_URL, $url);
+      curl_setopt($curl, CURLOPT_POST, true);
+      curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+      curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+      // curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+      
+      // Ejecutar la solicitud y obtener la respuesta
+      $response11 = curl_exec($curl);
+
+      
+
+
+      curl_close($curl);
+
+      
+
+        // Realizar acciones basadas en los valores de los encabezados
+
+
+        if ($response11 == 'true' ) {
+
+
+
+            $clientId= Flight::request()->data->clientId;
+            $comments= Flight::request()->data->comments;
+            $name= Flight::request()->data->name;
+             $caract= Flight::request()->data->caract;
+             $brand= Flight::request()->data->brand;
+             $type= Flight::request()->data->type;
+              $img= Flight::request()->data->img;
+
+
+            require_once '../../apiCompanies/v1/model/modelSecurity/uuid/uuidd.php';
+           
+   
+
+            $gen_uuid = new generateUuid();
+            $myuuid = $gen_uuid->guidv4();
+         
+
+            $elementId = substr($myuuid, 0, 8);
+
+         
+            $conectar=conn();
+
+           
+            $query= mysqli_query($conectar,"INSERT INTO clientElements (elementId,elementName,caracts,comments,brand,type,clientId,imgElements) VALUES ('$elementId','$name','$caract','$comments','$brand','$type','$clientId','$img')");
+            echo "true|¡Elemento creado con exito!";
+     
+
+           
+     
+
+       
+        
+           // echo json_encode($response1);
+        } else {
+            echo 'false|¡Autenticación fallida!';
+           // echo json_encode($data);
+        }
+    } else {
+        echo 'false|¡Encabezados faltantes!';
+    }
+});
+
+
 Flight::route('GET /getCalendarDays/@filter', function ($filter) {
     header("Access-Control-Allow-Origin: *");
     // Leer los encabezados
@@ -954,6 +1052,101 @@ Flight::route('GET /getClientRooms/@filter', function ($filter) {
                 $row=$query->fetch_assoc();
                 //echo json_encode($students) ;
                 echo json_encode(['clientRoom'=>$values]);
+          
+               
+           
+
+        } else {
+            echo 'Error: Autenticación fallida';
+             //echo json_encode($response1);
+        }
+    } else {
+        echo 'Error: Encabezados faltantes';
+    }
+});
+
+
+
+Flight::route('GET /getClientElements/@filter', function ($filter) {
+    header("Access-Control-Allow-Origin: *");
+    // Leer los encabezados
+    $headers = getallheaders();
+    
+    // Verificar si los encabezados 'Api-Key' y 'Secret-Key' existen
+    if (isset($headers['Api-Key']) && isset($headers['x-api-Key'])) {
+        // Leer los datos de la solicitud
+       
+        // Acceder a los encabezados
+        $apiKey = $headers['Api-Key'];
+        $xApiKey = $headers['x-api-Key'];
+        
+        $sub_domaincon=new model_domain();
+        $sub_domain=$sub_domaincon->domKairos();
+        $url = $sub_domain.'/kairosCore/apiAuth/v1/authApiKeyKairos/';
+      
+        $data = array(
+          'apiKey' =>$apiKey, 
+          'xApiKey' => $xApiKey
+          
+          );
+      $curl = curl_init();
+      
+      // Configurar las opciones de la sesión cURL
+      curl_setopt($curl, CURLOPT_URL, $url);
+      curl_setopt($curl, CURLOPT_POST, true);
+      curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+      curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+      // curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+      
+      // Ejecutar la solicitud y obtener la respuesta
+      $response1 = curl_exec($curl);
+
+      
+
+
+      curl_close($curl);
+
+      
+
+        // Realizar acciones basadas en los valores de los encabezados
+
+
+        if ($response1 == 'true' ) {
+           
+
+
+
+           
+            $conectar=conn();
+      
+          
+                $query= mysqli_query($conectar,"SELECT elementId,elementName,caracts,comments,isActive,status,barnd,type,clientId,isApply,imgElements FROM clientElements WHERE clientId='$filter'");
+           
+          
+                $values=[];
+          
+                while($row = $query->fetch_assoc())
+                {
+                        $value=[
+                            'elementId' => $row['elementId'],
+                            'elementName' => $row['elementName'],
+                            'caracts' => $row['caracts'],
+                            'comments' => $row['comments'],
+                            'isActive' => $row['isActive'],
+                            'status' => $row['status'],
+                            'brand' => $row['brand'],
+                            'type' => $row['type'],
+                            'clientId' => $row['clientId'],
+                            'isApply' => $row['isApply'],
+                            'imgElements' => $row['imgElements']
+                        ];
+                        
+                        array_push($values,$value);
+                        
+                }
+                $row=$query->fetch_assoc();
+                //echo json_encode($students) ;
+                echo json_encode(['clientElement'=>$values]);
           
                
            
